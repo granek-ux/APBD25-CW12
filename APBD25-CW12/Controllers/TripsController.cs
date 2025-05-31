6d457aed13f08bed3f02e82a185673efe9d1d0c2
granek-ux <kuba.balina@gmail.com>
2025-05-31 16:00:07 +0200

    Empty Controllers

---
+++ APBD25-CW12/Controllers/TripsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD25_CW12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        
+    }
+}
